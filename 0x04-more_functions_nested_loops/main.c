eb1e1f29af962943444ddf80249ed9e4d22e8e71
Mejda20 <guediri.majouda@gmail.com>
2023-04-01 15:12:01 +0100

    Initial commit

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Description: Program Starting Function
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	_putchar(65);
+	return (0);
+}
